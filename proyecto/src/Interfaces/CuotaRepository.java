c16b0465a5d6f57a9d6de4718dd4aeff24e64965
alexmillano <159828484+alexmillano@users.noreply.github.com>
2024-05-16 12:35:00 -0300

    Modifique comentario en repositorio cuota
    Modifique los comentarios que tenia dentro del repositorio de cuota

--- proyecto/src/Interfaces/CuotaRepository.java
+++ proyecto/src/Interfaces/CuotaRepository.java
@@ -5,13 +5,13 @@ import java.util.List;
 import Modelo.Cuota;
 
 public interface CuotaRepository {
-List<Cuota> getAllCuota(); // llama a todos las clientes de la bdd
+List<Cuota> getAllCuota(); // llama a todos las cuotas de la bdd
     
     Cuota getCuotaById(int id); //llama solo a uno, por su id
     
-    void addCuota(Cuota cuota); //añade clase a la bdd
+    void addCuota(Cuota cuota); //añade cuota a la bdd
     
-    void updateCuota(Cuota cuota); //actualiza las clase de la bdd
+    void updateCuota(Cuota cuota); //actualiza las cuotas de la bdd
     
-    void deleteCuota(int id); //eliminar clase de la bdd
+    void deleteCuota(int id); //eliminar cuota de la bdd
 }
